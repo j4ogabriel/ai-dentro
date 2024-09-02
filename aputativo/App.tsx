285c2c8c89dcd6bd654d64d95ee66c5009c0c9a6
j4ogabriel <jgcduailibe15@gmail.com>
2024-09-02 11:55:45 -0300

    lrifhkv
    dfghjk

---
+++ aputativo/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bem vindo ao Aputativo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
